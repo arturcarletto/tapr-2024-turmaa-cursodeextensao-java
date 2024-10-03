3474a5f9e0666235b5ce436e2b8273f5a7e5c892
AUGUSTO RECH <augusto.rech@univille.br>
2024-10-02 21:17:05 -0300

    first commit

---
+++ src/main/java/br/univille/microservcursodeextensao/MicroservcursodeextensaoApplication.java
@@ -0,0 +1,13 @@
+package br.univille.microservcursodeextensao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservcursodeextensaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservcursodeextensaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/univille/microservcursodeextensao/MicroservcursodeextensaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.univille.microservcursodeextensao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservcursodeextensaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
